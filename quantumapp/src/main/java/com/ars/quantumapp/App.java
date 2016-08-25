9a8aa49e063ed474bb6a793cf91a1c8dfe2a2b81
mihai <mihai@DESKTOP-FHRFBBT>
2016-08-25 17:53:00 +0300

    added grover's algorithm sketch

--- quantumapp/src/main/java/com/ars/quantumapp/App.java
+++ quantumapp/src/main/java/com/ars/quantumapp/App.java
@@ -1,12 +1,16 @@
 package com.ars.quantumapp;
 
+import com.ars.algorithms.deutsch.GroversAlgorithm;
+
 public class App {
 	private static Thread runner;
 	public static void main(String[] args) {
-		runner=new Thread(new DeutschRunner());
-		runner.start();
-		
-
+//		runner=new Thread(new DeutschRunner());
+//		runner.start();
+		GroversAlgorithm ga=new GroversAlgorithm();
+		ga.init();
+		ga.run();
+		ga.measure();
 	}
 
 }
